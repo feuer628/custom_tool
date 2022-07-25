85c44b2c26f1c6ebe33658c4e03c94e41ab7c9d2
Cheremnov <cheremnov@bifit.com>
2022-07-25 13:25:49 +0300

    Удаление копирайта

--- src/java/tools/CustomFilesTool.java
+++ src/java/tools/CustomFilesTool.java
@@ -1,18 +1,3 @@
-/*
- * STRICTLY CONFIDENTIAL
- * TRADE SECRET
- * PROPRIETARY:
- *       "BIFIT" JSC, TIN 7719617469
- *       105203, Russia, Moscow, Nizhnyaya Pervomayskaya, 46
- * (c) "BIFIT" JSC, 2022
- *
- * СТРОГО КОНФИДЕНЦИАЛЬНО
- * КОММЕРЧЕСКАЯ ТАЙНА
- * СОБСТВЕННИК:
- *       АО "БИФИТ", ИНН 7719617469
- *       105203, Россия, Москва, ул. Нижняя Первомайская, д. 46
- * (c) АО "БИФИТ", 2022
- */
 package tools;
 
 import java.io.*;
